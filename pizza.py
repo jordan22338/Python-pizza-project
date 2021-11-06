94169f1fbcd67f93255a882b830365648181dc90
jordan22338 <83674185+jordan22338@users.noreply.github.com>
2021-11-05 22:27:18 -0600

    Add files via upload

---
+++ pizza.py
@@ -0,0 +1,15 @@
+# Your code below:
+toppings=["pepperoni","pineapple","cheese","sausage","olives","anchovies","mushrooms"]
+prices=[2,6,1,3,2,7,2]
+num_two_dollar_slices=prices.count(2)
+num_pizzas=len(toppings)
+print("we sell" +str(num_pizzas)+ "different kinds of pizzas")
+pizza_and_prices=[[2,"pepperoni"],[6,"pineapple"],[1,"cheese"],[3,"sasuage"],[,2,"olives"],[7,"anchovives"],[2,"mushrooms"]]
+pizza_and_prices.insert(2.5,"peppers")
+three_cheapest=pizza_and_prices[:4]
+pizza_and_prices.sort()
+cheapest_pizza=pizza_and_prices(0)
+priciest_pizza=pizza_and_prices(-1)
+pizza_and_prices.pop("anchovives")
+print(pizza_and_prices)
+print(three_cheapest)
